b9310ee05251fbe6369f2096bc9a71327bc04f84
coding-learn11 <82395877+coding-learn11@users.noreply.github.com>
2023-06-15 09:52:08 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-https://teachablemachine.withgoogle.com/models/P6i04FTXg/model.json
+Webcam.set({
+    width: 350,
+    height: 270,
+    image_format: 'png',
+    png_quality: 90
+});
 
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function capture_image() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/P6i04FTXg/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded')
+}
